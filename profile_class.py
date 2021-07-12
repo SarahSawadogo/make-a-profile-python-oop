d9a91e1276f3340c67d95fcfde0df4677a4e5796
Sarah <sawadogosarah@gmail.com>
2021-07-12 17:30:31 +0000

    1:30

--- profile_class.py
+++ profile_class.py
@@ -1,2 +1,8 @@
 # View the readme on Github to get more information on how to start this definition. 
 class Profile:
+   def __init__(self, name, age, savings, plays_tennis):
+        self.name = name 
+        self.age = age
+        self.savings = savings
+        self.plays_Tennis = plays_tennis
+        print("Profile created")
--- test.py
+++ test.py
@@ -1,2 +1,9 @@
 # View the readme on GitHub for more information about how to start this challenge. 
 import profile_class
+
+nicki = profile_class.Profile("Nicki", 23, 3001.12, False)
+print (nicki.savings)
+sarah = profile_class.Profile("Sarah",16, 2045.32, True)
+print (sarah.name)
+erick = profile_class.Profile("Erick", 56, 6065.32, False)
+print (erick.plays_Tennis)
